55171c02cc6188846725ec6f891049a6b5b18c70
Mathreyu <Midna1213!>
2016-08-12 09:12:40 -0700

    Drawer initialized not implemented, added libraries for Stetho and Realm.

--- app/src/main/java/com/example/ramon/sessioncontrol/MainLogged.java
+++ app/src/main/java/com/example/ramon/sessioncontrol/MainLogged.java
@@ -3,8 +3,11 @@ package com.example.ramon.sessioncontrol;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import butterknife.BindView;
 
@@ -14,6 +17,10 @@ import butterknife.BindView;
 public class MainLogged extends Activity{
     @BindView(R.id.logout) Button logout;
     @BindView(R.id.exit) Button close;
+    @BindView(R.id.drawer_layout) DrawerLayout userInfoDrawer;
+    @BindView(R.id.left_drawer) ListView userInfoList;
+    private String[] optionItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,13 @@ public class MainLogged extends Activity{
 
         logout = (Button) findViewById(R.id.logout);
         close = (Button) findViewById(R.id.exit);
+
+        initDrawer();
+    }
+
+    private void initDrawer() {
+        userInfoList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, optionItems ));
+        
     }
 
     public void logout(View view){
