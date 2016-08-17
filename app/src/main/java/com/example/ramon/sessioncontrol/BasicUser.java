d600ca4612472ac76edf10cf4a77c6d77e063d54
Mathreyu <Midna1213>
2016-08-17 12:14:26 -0700

    Version 1.0  with notes but no DB for note persistance after logout. Will resume work later on.

--- app/src/main/java/com/example/ramon/sessioncontrol/BasicUser.java
+++ app/src/main/java/com/example/ramon/sessioncontrol/BasicUser.java
@@ -3,6 +3,8 @@ package com.example.ramon.sessioncontrol;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ramon on 8/9/2016.
  */
@@ -13,8 +15,19 @@ public class BasicUser {
     @Expose
     @SerializedName("password")
     String password;
+    @Expose
+    @SerializedName("Notes")
+    ArrayList<String> notesList;
 
-//region getters and setters
+    //region getters and setters
+    public ArrayList<String> getNotesList() {
+        return notesList;
+    }
+
+    public void setNotesList(ArrayList<String> notesList) {
+        this.notesList = notesList;
+    }
+
     public String getName() {
         return name;
     }
--- app/src/main/java/com/example/ramon/sessioncontrol/MainLogged.java
+++ app/src/main/java/com/example/ramon/sessioncontrol/MainLogged.java
@@ -1,12 +1,14 @@
 package com.example.ramon.sessioncontrol;
 
-import android.app.Activity;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -14,8 +16,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
@@ -30,17 +34,31 @@ public class MainLogged extends AppCompatActivity{
     @BindView(R.id.appbar) Toolbar appbar;
     @BindView(R.id.scrimInsetsFrameLayout) ScrimInsetsFrameLayout sifl;
     @BindView(R.id.navdrawerlist) ListView ndList;
+    @BindView(R.id.noteList) ListView noteListView;
 
+    private ArrayList<String> noteList;
     private ActionBarDrawerToggle drawerToggle;
+    private ArrayAdapter<String> listAdapter;
+    public static final String MyPreferences = "MyPreferences";
+
+    SharedPreferences UserLog;
+    SharedPreferences.Editor UserLogEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        UserLog = getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+
+        noteList = new ArrayList<>();
+        noteList.add("Example note");
+
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, noteList);
+
         setContentView(R.layout.activity_logged);
         ButterKnife.bind(this);
         setSupportActionBar(appbar);
 
-        final String[] options = new String[]{"Logout", "Exit App"};
+        final String[] options = new String[]{"","","","","","Logout", "Exit App"};
 
         ArrayAdapter<String> ndMenuAdapter =
                 new ArrayAdapter<String>(this,
@@ -52,10 +70,10 @@ public class MainLogged extends AppCompatActivity{
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position){
-                    case 0:
+                    case 5:
                         logout();
                         break;
-                    case 1:
+                    case 6:
                         close();
                         break;
                 }
@@ -88,6 +106,7 @@ public class MainLogged extends AppCompatActivity{
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -97,7 +116,7 @@ public class MainLogged extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -114,8 +133,31 @@ public class MainLogged extends AppCompatActivity{
 
         int id = item.getItemId();
 
-        if(id == R.id.action_settings){
-            return true;
+        noteListView.setAdapter(listAdapter);
+        if(id == R.id.action_add_note){
+            final EditText taskEditText = new EditText(this);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Add a note")
+                    .setView(taskEditText)
+                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String note = taskEditText.getText().toString();
+                            noteList.add(note);
+                            BasicUser user = new BasicUser();
+                            user.setNotesList(noteList);
+                            String UserInfoGson = new Gson().toJson(user);
+
+                            UserLogEditor = UserLog.edit();
+                            UserLogEditor.putString("UserInfo" , UserInfoGson);
+                            UserLogEditor.apply();
+                            listAdapter.notifyDataSetChanged();
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+            dialog.show();
+
         }
 
         return super.onOptionsItemSelected(item);
